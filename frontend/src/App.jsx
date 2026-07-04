fa1f039b1bf712f9fe95382a7aea1b856bcc0634
Victor Manuel Valera Castillo <victormvalera@gmail.com>
2026-07-04 13:11:40 -0400

    feat: crea frontend base de Aelius

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Aelius</h1>
+
+      <p>
+        Arquitectura a proyectar. Personas primero. Tecnología después.
+      </p>
+    </main>
+  )
+}
+
+export default App
